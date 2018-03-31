405814f186705d8dca9ae21c2e3097193400ffcb
whitphoto <nate@whitchurchphotography.com>
2018-03-30 20:18:09 -0400

    Homework 2-2 Web Browser

---
+++ ReadMe.md
@@ -0,0 +1,12 @@
+/********************************************************
+*														*
+*		Nathan Whitchurch								*
+*		CIS 223 CIS223: COMPUTER PROJECTS & APPLICATNS	*
+*		Dr Whittle										*
+*		Homework 2-2 Web Browser						*
+*														*
+********************************************************/
+
+Instructions
+2.	 Complete the following tutorial to create a web browser app: 
+http://msdn.microsoft.com/en-US/library/windowsphone/develop/ff402526(v=vs.105).aspx
---
+++ WebView/MainPage.xaml.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace WebView
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+
+        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void Go_Click(object sender, RoutedEventArgs e)
+        {
+            string site = URL.Text;
+            MiniBrowser.Navigate(new Uri(site, UriKind.Absolute));
+        }
+
+        private void MaristButton_Click(object sender, RoutedEventArgs e)
+        {
+            MiniBrowser.Navigate(new Uri("https://www.marist.edu/", UriKind.Absolute));
+        }
+
+        private void VassarButton_Click(object sender, RoutedEventArgs e)
+        {
+            MiniBrowser.Navigate(new Uri("https://www.vassar.edu/", UriKind.Absolute));
+        }
+    }
+}
